0185884218559ff040aceb394e99eeec5c68b3b6
Dimitrii Fominykh <a@webheads.ru>
2024-12-24 16:06:00 +0200

    Вывод баннера

--- include/mainpage/components/ws_banner-vip-client/text.php
+++ include/mainpage/components/ws_banner-vip-client/text.php
@@ -1,14 +1,51 @@
+<?php
+
+use Bitrix\Main\Loader;
+use Bitrix\Iblock\ElementTable;
+
+Loader::includeModule('iblock');
+
+// Задаем параметры
+$elementId = 742;
+$iblockId = 17;
+
+// Получаем элемент
+$res = CIBlockElement::GetByID($elementId);
+if ($element = $res->GetNext()) {
+    // ID детальной картинки
+    $detailPictureId = $element['DETAIL_PICTURE'];
+    $previewPictureId = $element['PREVIEW_PICTURE'];
+    $text = $element['NAME'];
+
+    // Получение путей к картинкам
+    $detailPicture = CFile::GetPath($detailPictureId);
+    $previewPicture = CFile::GetPath($previewPictureId);
+
+}
+?>
+
 <div class="mainpage-blocks ws-index-banner ws-index-banner__vip-client">
     <div class="ws-index-banner">
 
         <div class="ws-index-banner__image">
             <img id="ws-index-banner__vip-client" src=""
-                 alt="Станьте ВИП клиентом в компании Дядя Лайфу" loading="lazy">
+                 alt="<?=$text?>" loading="lazy">
         </div>
 
     </div>
 </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            let imgElement = document.getElementById('ws-index-banner__vip-client');
 
+            if (window.innerWidth <= 767) {
+                imgElement.src = "<?=$previewPicture?>";
+            } else {
+                imgElement.src = "<?=$detailPicture?>";
+            }
+        });
+    </script>
+<?/*?>
 <script>
     document.addEventListener("DOMContentLoaded", function () {
         let imgElement = document.getElementById('ws-index-banner__vip-client');
@@ -20,3 +57,4 @@
         }
     });
 </script>
+<?*/?>
